d46071cad2d4ccbf888b0078d7f577ef8fe2caf1
Mohamad-Mahmoud <mohamad_m@outlook.de>
2025-11-30 20:13:37 +0100

    added mongodb, restructre folders, added navbar.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:lAncDDLCLjXTTPwrKRoxWT5G@172.21.186.217:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
